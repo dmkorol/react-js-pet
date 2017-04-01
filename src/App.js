6be322ce5f2ed0553353da90abac517493aca900
Dmytro Korol <dkorol@gloriumtech.com>
2017-04-01 14:40:20 +0300

    init commint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Left-menu></Left-menu>
+        div
+    );
+  }
+}
+
+export default App;
